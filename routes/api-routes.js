24fb5f76028c613aecf173226007eac7fc73c2cb
ssimmons122 <121777930+ssimmons122@users.noreply.github.com>
2023-04-10 23:22:16 -0400

    routes started

---
+++ routes/api-routes.js
@@ -0,0 +1,9 @@
+const fs = require ("fs"); //
+let data = json.parse(fs.readFileSync("./db.json", "utf8")); // fix dbjson route
+
+module.exports = function(app) {
+    app.get("/api/notes", function(req, res) {
+       
+        res.json(data);
+    });
+}

1b0f527eabe2d75cd5540bcf24c042c50b550c78
ssimmons122 <121777930+ssimmons122@users.noreply.github.com>
2023-04-11 22:23:49 -0400

    const routes

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const express = require ('express');
 const fs = require ('fs');
 const path = require ('path');
+const htmlRoutes = require ("./routes/html-route");
+const apiRoutes = require ("./routes/api-routes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-//middleware for parsing JSON
+//middleware for parsing app/json
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('public', express.static(`${__dirname}/public`));
-
-require("./routes/html-routes")(app);
-require("./routes/api-routes")(app);
-
 
 // Starts the server to begin listening
 app.listen(PORT, function() {
